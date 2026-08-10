84b28fd6c638c57c6e8be5379550c86d95215197
Ahmed Wasfy <ahmed.wasfy@envogreen.net>
2026-08-10 14:44:38 -0400

    setup docker and intialize database schema

--- src/main.py
+++ src/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 
 from routes import base
 from routes import data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import Settings
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = Settings()
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongodb = app.mongodb_client[settings.MONGODB_DATABASE]
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
 
 app.include_router(base.base_Router)
 app.include_router(data.data_router)
